899404b6b52ccdbaf91aa8cfc84f24e961b609cd
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-04 16:08:40 +0900

    Initial Commit'

--- ILoggerMocking/Program.cs
+++ ILoggerMocking/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Moq;
+using System;
 
-namespace ILoggerMocking
+namespace ConsoleApp18
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var moq = new Mock<ILogger>();
+            moq.Setup(x => x.Trace("期待してるメッセージ", LogLevel.Info))
+                .Verifiable();
+
+            var logger = moq.Object;
+            logger.Info("期待してるメッセージ");  // If you comment this, you'll get MoqException.
+            moq.Verify();
         }
     }
+
+    public interface ILogger
+    {
+        void Trace(string message, LogLevel logLevel);
+    }
+
+    public static class LoggerExtensions
+    {
+        public static void Info(this ILogger self, string message)
+        {
+            self.Trace(message, LogLevel.Info);
+        }
+    }
+
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error
+    }
 }
+
