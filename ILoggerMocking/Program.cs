5b7b8ef32939006dc7a2c7ae347f796cc49ab101
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-04 15:41:18 +0900

    Add project files.

---
+++ ILoggerMocking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ILoggerMocking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
